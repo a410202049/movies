e19191ca6786f3f56ecc14930db98f1db147fb35
kerrygao <1509699669@qq.com>
2016-09-13 14:47:28 +0800

    ad

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://127.0.0.1/movie/';
+$config['base_url'] = 'http://www.movies.com';
 
 /*
 |--------------------------------------------------------------------------
@@ -369,8 +369,7 @@ $config['encryption_key'] = 'aab3162f9de3bdd00f7dd52662fed21d';
 */
 $config['sess_driver'] = 'files';
 $config['sess_cookie_name'] = 'ci_session';
-$config['sess_expiration'] = 7200;
-// $config['sess_save_path'] = NULL;
+$config['sess_expiration'] = 0;
 $config['sess_save_path'] = FCPATH.'public/sess_save_path';
 $config['sess_match_ip'] = FALSE;
 $config['sess_time_to_update'] = 300;
