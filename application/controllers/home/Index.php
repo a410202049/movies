8fbe40abc3d903357d9789c758c0cfcddc2d6c9e
kerrygao <1509699669@qq.com>
2016-09-28 15:37:37 +0800

    ad

--- application/controllers/home/Index.php
+++ application/controllers/home/Index.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Index extends CI_Controller {
+class Index extends Home_Base_Controller {
 
 	public function __construct(){
 		parent::__construct();
@@ -9,7 +9,6 @@ class Index extends CI_Controller {
 
 	public function index()
 	{
-		set_time_limit(0) ;
-		
+		$this->twig->render('Index/index');
 	}
 }
